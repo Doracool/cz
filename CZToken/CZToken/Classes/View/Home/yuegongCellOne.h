c4180afd865e1dbcb26e28a0c427126d09421c19
周帅杰 <ighibli@iGhibliMBP.local>
2018-02-27 22:05:48 +0800

    添加月供cell1

---
+++ CZToken/CZToken/Classes/View/Home/yuegongCellOne.h
@@ -0,0 +1,13 @@
+//
+//  yuegongCellOne.h
+//  CZToken
+//
+//  Created by 周帅杰 on 2018/2/27.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface yuegongCellOne : UITableViewCell
+
+@end
---
+++ CZToken/CZToken/Classes/View/Home/yuegongCellOne.m
@@ -0,0 +1,24 @@
+//
+//  yuegongCellOne.m
+//  CZToken
+//
+//  Created by 周帅杰 on 2018/2/27.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import "yuegongCellOne.h"
+
+@implementation yuegongCellOne
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
