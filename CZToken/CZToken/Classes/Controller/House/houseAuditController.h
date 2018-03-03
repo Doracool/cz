aaedb4811cc1e819e654a54fe7ebf03f8ee89dce
doracool <asas13525482040@163.com>
2018-03-03 18:30:10 +0800

    添加审核Controller

---
+++ CZToken/CZToken/Classes/Controller/House/houseAuditController.h
@@ -0,0 +1,13 @@
+//
+//  houseAuditController.h
+//  CZToken
+//
+//  Created by jlc on 2018/3/3.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface houseAuditController : UIViewController
+
+@end
---
+++ CZToken/CZToken/Classes/Controller/House/houseAuditController.m
@@ -0,0 +1,37 @@
+//
+//  houseAuditController.m
+//  CZToken
+//
+//  Created by jlc on 2018/3/3.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import "houseAuditController.h"
+
+@interface houseAuditController ()
+
+@end
+
+@implementation houseAuditController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
