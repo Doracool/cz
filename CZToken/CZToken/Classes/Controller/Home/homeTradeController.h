8be062c42284429b4650c2afc06907ecaf1ae31c
周帅杰 <ighibli@iGhibliMBP.local>
2018-03-20 22:58:08 +0800

    新添加交易界面

---
+++ CZToken/CZToken/Classes/Controller/Home/homeTradeController.h
@@ -0,0 +1,29 @@
+//
+//  homeTradeController.h
+//  CZToken
+//
+//  Created by 周帅杰 on 2018/3/20.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface homeTradeController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIView *view2;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *view2Height;
+@property (weak, nonatomic) IBOutlet UIView *view2Small;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *smallHeight;
+@property (weak, nonatomic) IBOutlet UIView *view3;
+@property (weak, nonatomic) IBOutlet UIView *view3Samll;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *view3Height;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *small2Height;
+@property (weak, nonatomic) IBOutlet UIView *view4;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *view4Height;
+@property (weak, nonatomic) IBOutlet UIView *view5;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *view5Height;
+@property (weak, nonatomic) IBOutlet UIButton *jsBtn;
+@property (weak, nonatomic) IBOutlet UIView *view6;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *view6Height;
+@end
---
+++ CZToken/CZToken/Classes/Controller/Home/homeTradeController.m
@@ -0,0 +1,37 @@
+//
+//  homeTradeController.m
+//  CZToken
+//
+//  Created by 周帅杰 on 2018/3/20.
+//  Copyright © 2018年 NYHZ. All rights reserved.
+//
+
+#import "homeTradeController.h"
+
+@interface homeTradeController ()
+
+@end
+
+@implementation homeTradeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
