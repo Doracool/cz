a6912e6896b460248ab1543740616b77e624fbcf
doracool <asas13525482040@163.com>
2018-03-22 18:26:57 +0800

    调试计算交易接口

--- CZToken/CZToken/Classes/Controller/Computer/calculateController.h
+++ CZToken/CZToken/Classes/Controller/Computer/calculateController.h
@@ -13,4 +13,18 @@
 @property (strong, nonatomic) IBOutlet UIButton *neihuan;
 @property (strong, nonatomic) IBOutlet UIButton *neiorwai;
 @property (strong, nonatomic) IBOutlet UIButton *waihuan;
+@property (strong, nonatomic) IBOutlet UIButton *bmln;
+@property (strong, nonatomic) IBOutlet UIButton *ezwn;
+@property (strong, nonatomic) IBOutlet UIButton *wnys;
+@property (strong, nonatomic) IBOutlet UIButton *wy;
+@property (strong, nonatomic) IBOutlet UIButton *bwy;
+@property (strong, nonatomic) IBOutlet UIButton *st;
+@property (strong, nonatomic) IBOutlet UIButton *bsst;
+@property (strong, nonatomic) IBOutlet UIButton *dk;
+@property (strong, nonatomic) IBOutlet UIButton *bdk;
+@property (strong, nonatomic) IBOutlet UIView *jylxView;
+@property (strong, nonatomic) IBOutlet UITextField *fwzjTf;
+@property (strong, nonatomic) IBOutlet UITextField *fwmjTf;
+@property (strong, nonatomic) IBOutlet UITextField *mjdjTf;
+@property (strong, nonatomic) IBOutlet UITextField *ssjgTf;
 @end
--- CZToken/CZToken/Classes/Controller/Computer/calculateController.m
+++ CZToken/CZToken/Classes/Controller/Computer/calculateController.m
@@ -9,22 +9,88 @@
 #import "calculateController.h"
 
 @interface calculateController ()
-
+@property (nonatomic, assign) int ring;
+@property (nonatomic, assign) int tradeType;
+@property (nonatomic, assign) int propertyRing;
+@property (nonatomic, assign) int isOnly;
+@property (nonatomic, assign) int isFirst;
+@property (nonatomic, assign) int isLoan;
+@property (nonatomic, assign) int HousePrice;
+@property (nonatomic, assign) int houseArea;
 @end
 
 @implementation calculateController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createBaseUI];
     // Do any additional setup after loading the view from its nib.
 }
+
+- (void)createBaseUI {
+    DVSwitch *typeOne = [[DVSwitch alloc] initWithStringsArray:@[@"一手",@"二手"]];
+    typeOne.frame = _jylxView.frame;
+    [_jylxView addSubview:typeOne];
+    typeOne.frame = CGRectMake(0, 0, _jylxView.width, _jylxView.height);
+    [typeOne setPressedHandler:^(NSUInteger index) {
+        NSLog(@"switch to index: %lu",(unsigned long)index);
+    }];
+}
+
 - (IBAction)jisuanAction:(UIButton *)sender {
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    [params setObject:TOKEN forKey:@"Token"];
+    [params setObject:@"ios" forKey:@"Source"];
+    [params setObject:@(1) forKey:@"HouseType2"];
+    [params setObject:@(_ring) forKey:@"Ring"];
+    [params setObject:@(1) forKey:@"TradeType"];
+    [params setObject:@(_isFirst) forKey:@"IsFirst"];
+    [params setObject:@(_isLoan) forKey:@"IsLoan"];
+    [params setObject:@([_fwmjTf.text doubleValue]) forKey:@"HouseSize"];
+    [params setObject:@([_fwzjTf.text doubleValue]) forKey:@"HouseTotalPrice"];
+    [params setObject:@([_mjdjTf.text doubleValue]) forKey:@"HousePrice"];
+    NSString *URL = [NSString stringWithFormat:@"%@/Trade/Taxation/CalHouseTaxation",BaseUrl];
+    URL = [URL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager POST:URL parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:nil];
+        NSLog(@"%@",dic);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
 }
 - (IBAction)selectHuan:(UIButton *)sender {
     _neihuan.selected = NO;
     _neiorwai.selected = NO;
     _waihuan.selected = NO;
     sender.selected = YES;
+    _ring = (int)sender.tag;
+}
+- (IBAction)selectChanquan:(UIButton *)sender {
+    _bmln.selected = NO;
+    _ezwn.selected = NO;
+    _wnys.selected = NO;
+    sender.selected = YES;
+    _propertyRing = (int)sender.tag;
+}
+- (IBAction)selectWeiyi:(UIButton *)sender {
+    _wy.selected = NO;
+    _bwy.selected = NO;
+    sender.selected = YES;
+    _isOnly = (int)sender.tag;
+}
+- (IBAction)selectShoutao:(UIButton *)sender {
+    _st.selected = NO;
+    _bsst.selected = NO;
+    sender.selected = YES;
+    _isFirst = (int)sender.tag;
+}
+- (IBAction)selectDaikuan:(UIButton *)sender {
+    _dk.selected = NO;
+    _bdk.selected = NO;
+    sender.selected = YES;
+    _isLoan = (int)sender.tag;
 }
 
 - (void)didReceiveMemoryWarning {
